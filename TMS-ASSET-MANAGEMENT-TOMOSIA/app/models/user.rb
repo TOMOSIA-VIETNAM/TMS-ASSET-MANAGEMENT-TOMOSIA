ebc24deb9d5eef3aaa2cd9aff2bdcbc927fb7f83
tms-ducthai <tt.duc.thai@tomosia.com>
2021-05-12 17:14:53 +0700

    gem-devise

--- TMS-ASSET-MANAGEMENT-TOMOSIA/app/models/user.rb
+++ TMS-ASSET-MANAGEMENT-TOMOSIA/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :projects
-  has_many :requests
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
