3f4b7cd2b0e413365d6b2880135abfb05488e1a8
tms-thiephuynh <tt.thiep.huynh@tomosia.com>
2021-05-17 10:41:27 +0700

    add pundit

--- TMS-ASSET-MANAGEMENT-TOMOSIA/app/models/user.rb
+++ TMS-ASSET-MANAGEMENT-TOMOSIA/app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
   has_many :projects
+  has_many :requests
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: {
+    user: 0,
+    manage: 1, 
+    admin: 2,
+  }
 end
