ef771c76f921ca7d3e65ca68fcc9aa795988fcac
rikai-thiephuynh <thiep.huynh@rikai.technology>
2021-05-31 21:25:40 +0700

    validate date

--- TMS-ASSET-MANAGEMENT-TOMOSIA/app/models/request.rb
+++ TMS-ASSET-MANAGEMENT-TOMOSIA/app/models/request.rb
@@ -1,6 +1,6 @@
 class Request < ApplicationRecord
   extend Enumerize
-  WEBHOOK_URL = 'https://hooks.slack.com/services/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'
+  WEBHOOK_URL = 'https://hooks.slack.com/services/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'
   
   belongs_to :user
   belongs_to :item
@@ -18,7 +18,10 @@ class Request < ApplicationRecord
     Break: 2
   }
 
-  validates :reason, :start_date, :end_date, presence: true
+  validates :reason,:start_date, :end_date, presence: true
+  
+  validate :start_date_is_equal_today
+  validate :end_date_is_after_start_date
 
   def self.search(term)
     if term
@@ -27,6 +30,22 @@ class Request < ApplicationRecord
       all
     end
 
+  end
+
+  def end_date_is_after_start_date
+    return if end_date.blank? || start_date.blank?
+  
+    if  start_date > end_date 
+      errors.add(:end_date, "cannot be before the start date") 
+    end 
+  end
+
+  def start_date_is_equal_today
+    return if start_date.blank?
+    to_date =  Time.zone.now
+    if start_date < to_date
+      errors.add(:start_date, " must not be less than current date") 
+    end 
   end
 
   scope :status_pending, -> {where status: 'pending'}
