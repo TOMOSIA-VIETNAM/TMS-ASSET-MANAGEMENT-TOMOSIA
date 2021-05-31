39fc8d61eed5d3e42095307a634846b0a00bacdf
tms-ducthai <tt.duc.thai@tomosia.com>
2021-05-31 16:43:04 +0700

    fix bug dashboard admin

--- TMS-ASSET-MANAGEMENT-TOMOSIA/app/controllers/admin/dashboards_controller.rb
+++ TMS-ASSET-MANAGEMENT-TOMOSIA/app/controllers/admin/dashboards_controller.rb
@@ -5,5 +5,19 @@ class Admin::DashboardsController < Admin::BaseController
     @items_broken = Item.items_broken
     @items_stock = Item.items_stock
     @items_out_stock = Item.items_out_stock
+
+    authorize @items_broken
+    authorize @items_stock
+    authorize @items_out_stock 
+  end
+
+  private
+
+  def policy_scope(scope)
+    super([:admin, scope])
+  end
+
+  def authorize(record, query = nil)
+    super([:admin, record], query)
   end
 end
